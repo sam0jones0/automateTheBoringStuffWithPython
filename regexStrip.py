3a40d5acf203df054336d8d09c18e8c1d19077cd
Sam Jones <sam0jones0@gmail.com>
2020-05-01 13:57:59 +0100

    Create regexStrip.py

---
+++ regexStrip.py
@@ -0,0 +1,19 @@
+#! python3
+# regexStrip.py - Uses regex to emulate the strip() function
+
+import re, pyperclip
+
+workingString = '          this is a test string.        '
+
+def regexStrip(string, sub=' '):
+    if len(sub) != 1:
+        print('Second argument must be only 1 character')
+        return 'Error'
+    else:
+        leadingSub = re.compile(rf'^({re.escape(sub)}+)([^{re.escape(sub)}].*)', re.DOTALL)
+        trailingSub = re.compile(rf'(.*[^{re.escape(sub)}])({re.escape(sub)}+)$', re.DOTALL)
+        string = leadingSub.sub(r'\2', string)
+        string = trailingSub.sub(r'\1', string)
+        return string
+
+print(regexStrip(workingString))
