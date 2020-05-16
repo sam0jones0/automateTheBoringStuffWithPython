c4e36f5b8e358c7df6d03ea5ba55f58e0e3e6fbe
Sam Jones <sam0jones0@gmail.com>
2020-05-16 13:41:37 +0100

    Create myFileBot.py

---
+++ myFileBot.py
@@ -0,0 +1,42 @@
+#! python3
+# myFileBot.py - Moves individual TV show files from Downloads 
+# folder to TV/{showname} subfolder
+# Usage - python myFileBot.py {showname}
+
+from pathlib import Path
+import os, sys, re, shutil
+
+showName = sys.argv[1]
+
+showFolderRegex = re.compile(rf'{re.escape(showName)}.*', re.I)   # Regex for TV show folder name
+
+showEpisodeRegex = re.compile(rf'''
+    ({re.escape(showName)})       # Show name as match group1
+    .*                            
+    (S\d\dE\d\d)                  # Season & episode number as match group2
+    .*
+    \.mkv                         # File suffix
+''', re.I | re.VERBOSE)
+
+downloadsFiles = os.listdir(Path('D:/Downloads'))    # Location of Download folder
+
+# Creates folder for files to be moved to D:/Documents/TV/{showname}
+tvFolder = (Path(f'D:/Documents/TV/{sys.argv[1].title()}'))    
+if not tvFolder.exists():
+    os.makedirs(tvFolder)
+
+for matches in downloadsFiles:
+    episodeFolder = showFolderRegex.search(matches)
+    if episodeFolder != None:
+        episodeSubFolder = os.listdir(Path(f'D:/Downloads/{episodeFolder.group()}/'))
+        for filesInSubfolder in episodeSubFolder:
+            episode = showEpisodeRegex.search(filesInSubfolder)
+            if episode != None:
+                # Matches season and episode number, e.g. S01E01
+                seasonAndEpisode = episode.group(2)
+                # Set episode path
+                episodePath = Path(f'D:/Downloads/{episodeFolder.group()}/{episode.group()}')
+                # Moves and renames episode in format {showname}SXXEXX
+                shutil.move(episodePath, tvFolder / f'{sys.argv[1].title()} {seasonAndEpisode}.mkv')
+                # Remove folder from Downloads folder
+                shutil.rmtree((Path(f'D:/Downloads/{episodeFolder.group()}')))
